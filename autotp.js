b9aeee74f11a0d1d28858438dcf5e2c39ee8a7eb
Perseus333 <perseusmith73@gmail.com>
2025-02-08 13:19:32 +0000

    feat: first working implementation (no encryption)

--- autotp.js
+++ autotp.js
@@ -1,6 +1,16 @@
-import { totp } from './totp.js';
+autofillTOTP();
 
-let key = await loadSecret();
+async function autofillTOTP() {
+    let key = await loadSecret();
+    let code = await totp(key);
+    console.log("Generated TOTP:", code);
+    waitForElement("#verificationCodeInput", inputField => {
+        inputField.value = code;
+        inputField.dispatchEvent(new Event('input', { bubbles: true }));
+        let form = inputField.closest("form");
+        if (form) form.submit();
+    });
+}
 
 function waitForElement(selector, callback, timeout = 10000) {
     const start = Date.now();
@@ -22,18 +32,42 @@ async function loadSecret() {
     return data.secret;
 }
 
-async function autofillTOTP() {
-    let code = await totp(key);
-    console.log("Generated TOTP:", code);
-    waitForElement("#verificationCodeInput", inputField => {
-        inputField.value = code;
-        inputField.dispatchEvent(new Event('input', { bubbles: true }));
-        let form = inputField.closest("form");
-        if (form) form.submit();
-    });
-}
-
-autofillTOTP();
-
 const observer = new MutationObserver(() => autofillTOTP());
 observer.observe(document.body, { childList: true, subtree: true });
+
+
+/* Original code from totp-in-javascript by turistu */
+
+async function totp(key, secs = 30, digits = 6){
+	return hotp(unbase32(key), pack64bu(Date.now() / 1000 / secs), digits);
+}
+async function hotp(key, counter, digits){
+	let y = self.crypto.subtle;
+	if(!y) throw Error('no self.crypto.subtle object available');
+	let k = await y.importKey('raw', key, {name: 'HMAC', hash: 'SHA-1'}, false, ['sign']);
+	return hotp_truncate(await y.sign('HMAC', k, counter), digits);
+}
+function hotp_truncate(buf, digits){
+	let a = new Uint8Array(buf), i = a[19] & 0xf;
+	return fmt(10, digits, ((a[i]&0x7f)<<24 | a[i+1]<<16 | a[i+2]<<8 | a[i+3]) % 10**digits);
+}
+
+function fmt(base, width, num){
+	return num.toString(base).padStart(width, '0')
+}
+function unbase32(s){
+    console.log("s:", s, typeof s);
+	let t = (s.toLowerCase().match(/\S/g)||[]).map(c => {
+		let i = 'abcdefghijklmnopqrstuvwxyz234567'.indexOf(c);
+		if(i < 0) throw Error(`bad char '${c}' in key`);
+		return fmt(2, 5, i);
+	}).join('');
+	if(t.length < 8) throw Error('key too short');
+	return new Uint8Array(t.match(/.{8}/g).map(d => parseInt(d, 2)));
+}
+function pack64bu(v){
+	let b = new ArrayBuffer(8), d = new DataView(b);
+	d.setUint32(0, v / 2**32);
+	d.setUint32(4, v);
+	return b;
+}
